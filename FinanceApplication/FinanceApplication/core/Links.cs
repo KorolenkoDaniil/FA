2e4dbf153a97db4bfd551cbecc4a8bd5aca7bc4a
korolenko_danila <korolenko.danila1@gmail.com>
2024-04-06 19:10:12 +0300

    corrected card page

--- FinanceApplication/FinanceApplication/core/Links.cs
+++ FinanceApplication/FinanceApplication/core/Links.cs
@@ -2,14 +2,14 @@
 {
     public static class Links
     {
-        public static string Registration = "https://dbeb-185-203-155-43.ngrok-free.app/Users/Registration";
-        public static string Authorisation = "https://dbeb-185-203-155-43.ngrok-free.app/Users/Authorisation";
-        public static string SaveWallet = "https://dbeb-185-203-155-43.ngrok-free.app/Wallets/RegisterAWallet";
-        public static string GetWallets = "https://dbeb-185-203-155-43.ngrok-free.app/Wallets/GetWallets";
-        public static string GetColor = "https://dbeb-185-203-155-43.ngrok-free.app/Color/GetColor";
-        public static string SaveOperation = "https://dbeb-185-203-155-43.ngrok-free.app/Operations/RegisterOperation"; 
-        public static string GetOperations = "https://dbeb-185-203-155-43.ngrok-free.app/Operations/GetOperations"; 
-        public static string SaveCategory = "https://dbeb-185-203-155-43.ngrok-free.app/Categories/RegisterCategory"; 
-        public static string GetCategories = "https://dbeb-185-203-155-43.ngrok-free.app/Categories/GetCategories"; 
+        public static string Registration = "https://7cf1-185-203-155-44.ngrok-free.app/Users/Registration";
+        public static string Authorisation = "https://7cf1-185-203-155-44.ngrok-free.app/Users/Authorisation";
+        public static string SaveWallet = "https://7cf1-185-203-155-44.ngrok-free.app/Wallets/RegisterAWallet";
+        public static string GetWallets = "https://7cf1-185-203-155-44.ngrok-free.app/Wallets/GetWallets";
+        public static string GetColor = "https://7cf1-185-203-155-44.ngrok-free.app/Color/GetColor";
+        public static string SaveOperation = "https://7cf1-185-203-155-44.ngrok-free.app/Operations/RegisterOperation"; 
+        public static string GetOperations = "https://7cf1-185-203-155-44.ngrok-free.app/Operations/GetOperations"; 
+        public static string SaveCategory = "https://7cf1-185-203-155-44.ngrok-free.app/Categories/RegisterCategory"; 
+        public static string GetCategories = "https://7cf1-185-203-155-44.ngrok-free.app/Categories/GetCategories"; 
     }
 }
